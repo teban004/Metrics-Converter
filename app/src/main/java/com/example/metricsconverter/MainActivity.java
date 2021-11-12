f82b0ed1905a3457702734fdff6599168f66de89
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-12 08:34:28 -0500

    added the Celsius to Fahrenheit formula

--- app/src/main/java/com/example/metricsconverter/MainActivity.java
+++ app/src/main/java/com/example/metricsconverter/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.metricsconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button convert;
+    EditText input;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        convert = findViewById(R.id.btnConvert);
+        input = findViewById(R.id.etxtInput);
+        result = findViewById(R.id.txtvResult);
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double inputValue, calculatedValue;
+                inputValue = Double.parseDouble(input.getText().toString());
+                calculatedValue = (inputValue - 32) * 5 / 9;
+                result.setText(String.valueOf(calculatedValue));
+            }
+        });
+
     }
 }
