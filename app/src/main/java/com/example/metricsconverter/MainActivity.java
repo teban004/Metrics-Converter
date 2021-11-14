d083e5913dfca15101a1aadcf19d2e3121d1dce1
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-11-14 07:40:45 -0500

    added TextChangedListener on Celsius input

--- app/src/main/java/com/example/metricsconverter/MainActivity.java
+++ app/src/main/java/com/example/metricsconverter/MainActivity.java
@@ -3,35 +3,54 @@ package com.example.metricsconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button convert;
-    EditText input;
-    TextView result;
+    EditText celsius;
+    TextView fahrenheit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        convert = findViewById(R.id.btnConvert);
-        input = findViewById(R.id.etxtInput);
-        result = findViewById(R.id.txtvResult);
+        celsius = findViewById(R.id.etxtCelsius);
+        fahrenheit = findViewById(R.id.etxtFahrenheit);
 
-        convert.setOnClickListener(new View.OnClickListener() {
+        celsius.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                double inputValue, calculatedValue;
-                inputValue = Double.parseDouble(input.getText().toString());
-                calculatedValue = (inputValue - 32) * 5 / 9;
-                result.setText(String.valueOf(calculatedValue));
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
-        });
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                float inputValue, calculatedValue;
+                if(celsius.getText() != null) {
+                    try {
+                        inputValue = Float.parseFloat(celsius.getText().toString());
+                        calculatedValue = (inputValue * 9 / 5) + 32;
+                        fahrenheit.setText(String.valueOf(calculatedValue));
+                    } catch (Exception ex) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "An exception occurred when calculating Fahrenheit value.", Toast.LENGTH_SHORT);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        } );
 
     }
 }
